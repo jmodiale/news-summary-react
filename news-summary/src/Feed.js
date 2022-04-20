ef46dc29069c42a4e25e837f07015d450c2ac7b6
remen <remen.omodiale@outlook.com>
2022-04-20 12:48:52 +0100

    search & rendering

--- news-summary/src/Feed.js
+++ news-summary/src/Feed.js
@@ -6,32 +6,49 @@ const api_key = 'f7952ab1-2fe1-4dbd-9dbb-f97bdc0f3fca'
 export default function Feed() {
     const url = `https://content.guardianapis.com/search?q=&query-fields=headline&show-fields=thumbnail,headline,byline&order-by=newest&api-key=${api_key}`;
 
-    const [data, setData] = useState('')
+    const [data, setData] = useState([])
+    const [entry, setEntry] = useState('')
+
+    const filtered = data.filter((elem) => {
+        const word = elem.fields.headline.toLowerCase()
+        if (word.includes(entry.toLowerCase())){
+            return word
+        } else {
+            return null
+        }
+    })
 
     useEffect(()=>{
         const fetchData = async () => {
             const res = await axios.get(url);
-            
             setData(res.data.response.results)
         }
-
         fetchData();
-    }, [])
+    }, [url])
 
-   
-    const newData = Object.entries(data)
-    console.log(newData);
+    console.log(entry)
+    console.log(data)
 
   return (
     <div>
+      <form type="submit">
+        <input value={entry} onChange={(e) => setEntry(e.target.value)} />
+      </form>
       <article className="articles">
-          <h1>The news</h1>
-        {/* {newData.map((item) => {
-          <article>
-            <a href={item.webUrl}>Web Page</a>
-            <p>{item.fields.headline}</p>
-          </article>
-        })} */}
+        {filtered.length !== 0 && (
+            <div>
+            {filtered.map((item) => {
+          return (
+            <article key={item.id}>
+              <a href={item.webUrl}>
+                <p>{item.fields.headline}</p>
+              </a>
+              <img src={item.fields.thumbnail} alt="pics" />
+            </article>
+          );
+        })}
+        </div>
+        )};
       </article>
     </div>
   );}
